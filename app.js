48c8a8227371eb5448facaad9fb3c6c647b16ebd
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-17 13:36:20 -0300

    N89 - serving static content

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+// MiddleWares
+// Servir contenido estatico
+app.use( express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Home page')
 })
@@ -11,7 +15,7 @@ app.get('/hello-world',  (req, res) =>{
 })
 
 app.get('*',  (req, res) =>{
-  res.send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () =>{

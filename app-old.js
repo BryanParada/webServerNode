645ec0889ae051332d9ebfb4c5d71783cc98beec
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-17 12:17:43 -0300

    N88 - ExpressJS

---
+++ app-old.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+http.createServer( (req, res) => {
+ 
+    // res.writeHead(200, { 'Content-Type': 'text/plain' } );
+    // res.writeHead(200, { 'Content-Type': 'application/json' } );
+
+    // const person = {
+    //     id: 1,
+    //     name: 'Bryan'
+    // }
+
+    // res.write(JSON.stringify(person)); 
+
+    //=================EXPORT CSV
+    // res.setHeader('Content-Disposition', 'attachment; filename=list.csv')
+    // res.writeHead(200, { 'Content-Type': 'application/csv' } );
+
+    // res.write('id; name\n');
+    // res.write('1; Juan\n');
+    // res.write('2; Eva\n');
+    // res.write('3; Pedro\n');
+    // res.end();
+
+    res.write('Hello world');
+    res.end();
+
+ 
+})
+.listen(8080);
+
+console.log('Listening in port', 8080); 
+
--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080;
 
-http.createServer( (req, res) => {
- 
-    // res.writeHead(200, { 'Content-Type': 'text/plain' } );
-    // res.writeHead(200, { 'Content-Type': 'application/json' } );
-
-    // const person = {
-    //     id: 1,
-    //     name: 'Bryan'
-    // }
-
-    // res.write(JSON.stringify(person)); 
-
-    res.setHeader('Content-Disposition', 'attachment; filename=list.csv')
-    res.writeHead(200, { 'Content-Type': 'application/csv' } );
-
-    res.write('id; name\n');
-    res.write('1; Juan\n');
-    res.write('2; Eva\n');
-    res.write('3; Pedro\n');
-    res.end();
-
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Home page')
 })
-.listen(8080);
 
-console.log('Listening in port', 8080); 
+app.get('/hello-world',  (req, res) =>{
+  res.send('Hello World')
+})
 
+app.get('*',  (req, res) =>{
+  res.send('404 | Page not found')
+})
+
+app.listen(port, () =>{
+  console.log(`Listent at http://localhost:${port}`);
+  
+})
+
+
+ 

211af62638707020014c488c5eaf6e85c2d0ad09
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-17 11:46:18 -0300

    N87 - Request Response export csv

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const http = require('http');
 
 http.createServer( (req, res) => {
+ 
+    // res.writeHead(200, { 'Content-Type': 'text/plain' } );
+    // res.writeHead(200, { 'Content-Type': 'application/json' } );
 
-    res.write('Hello');
+    // const person = {
+    //     id: 1,
+    //     name: 'Bryan'
+    // }
+
+    // res.write(JSON.stringify(person)); 
+
+    res.setHeader('Content-Disposition', 'attachment; filename=list.csv')
+    res.writeHead(200, { 'Content-Type': 'application/csv' } );
+
+    res.write('id; name\n');
+    res.write('1; Juan\n');
+    res.write('2; Eva\n');
+    res.write('3; Pedro\n');
+    res.end();
+
     res.end();
 })
 .listen(8080);
 
-console.log('Listening in port', 8080);
+console.log('Listening in port', 8080); 
+
